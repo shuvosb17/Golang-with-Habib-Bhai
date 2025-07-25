d065dfe5792a271f6c5d98d56eb400c8fe6b98e8
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-07-25 12:40:58 +0600

    Create main.go

---
+++ [Golang] 015 - Local Scope and Block/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var (
+	a = 10
+	b = 20
+)
+
+func main() {
+	x := 15
+
+	if x >= 12 {
+		p := 17
+		fmt.Println("I am matured")
+		fmt.Println("I will get", p, "Girlfriends")
+	}
+}
